50617642cce2b2e47f9d0fcc66fd949715d6356e
JR Minnaar <jr.minnaar@gmail.com>
2016-12-22 14:52:00 +0200

    Fix colours and merge dual axis chart

--- superset/assets/javascripts/explorev2/stores/fields.js
+++ superset/assets/javascripts/explorev2/stores/fields.js
@@ -78,6 +78,14 @@ export const fields = {
     description: 'Choose the metric',
   },
 
+  metric_2: {
+    type: 'SelectField',
+    label: 'Right Axis Metric',
+    choices: [],
+    default: null,
+    description: 'Choose a metric for right axis',
+  },
+
   stacked_style: {
     type: 'SelectField',
     label: 'Stacked Style',
@@ -647,6 +655,14 @@ export const fields = {
   y_axis_format: {
     type: 'FreeFormSelectField',
     label: 'Y axis format',
+    default: '.3s',
+    choices: D3_TIME_FORMAT_OPTIONS,
+    description: D3_FORMAT_DOCS,
+  },
+
+  y_axis_2_format: {
+    type: 'FreeFormSelectField',
+    label: 'Right Axis Format',
     default: '.3s',
     choices: D3_TIME_FORMAT_OPTIONS,
     description: D3_FORMAT_DOCS,
